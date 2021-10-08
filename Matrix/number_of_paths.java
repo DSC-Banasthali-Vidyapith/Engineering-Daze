257657229473889137c3c21cfa8428e53b3805e7
tmehrotra <tanu_mehrotra@intuit.com>
2021-10-08 20:31:01 +0530

    Indentation corrcetions

--- Matrix/number_of_paths.java
+++ Matrix/number_of_paths.java
@@ -11,24 +11,25 @@ public class number_of_paths
 
 	/* Explanation:
 	Based on the contraints in the question, from every cell, we can either move right or down. 
-	Thinking in a bottom up manner, if we are at cell[i][j], we would have come adjacent cell above or adjacent left cell.
+	Thinking in a bottom up manner, if we are at cell[i][j], we would have come from either adjacent cell above or adjacent left cell.
 
-	Thus, number ways for cell[i][j] would be - 
+	Thus, number of ways to reach cell[i][j] would be - 
 	
-				  above cell      left cell
+			above cell      left cell
 	cell[i][j] = cell[i-1][j] + cell[i][j-1];
 
 	To reach starting cell - (0,0) - number of ways = 1.
-	So to reach cell(0,1) would be = cell(-1,0) + cell(0,0)
-										X       + 1
-									= 1
+	So to reach cell(0,1) would be (using recursion defined above)
+	= cell(-1,1) + cell(0,0)
+	=     X      +     1
+	=       1
 
 	The recursion shows that we can divide the problem in optimal subproblems and those sub problems are overlapping. 
-	Therefore we can solve this optimally using Dynamic Programming + Memozation.
+	Therefore we can solve this optimally using Dynamic Programming + Memoization.
 
-	The below solution follows bottom up approach - 
+	The below solution follows the bottom up approach for DP - 
 		1. start by defining the base cases
-		2. Iterate over the grid and calculate number of ways @ cell[i][j] using the abvove defined recursion
+		2. Iterate over the grid and calculate number of ways @ cell[i][j] using the above defined recursion
 		3. Values are stored in dp[i][j] for achieving memoization
 	*/
 
@@ -61,13 +62,11 @@ public class number_of_paths
 
 	public static void main(String[] args)
 	{
-
 		Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number of rows in matrix");
-        int m=sc.nextInt();
-        System.out.println("Enter number of columns in matrix");
-        int n=sc.nextInt();
-
+		System.out.println("Enter number of rows in matrix");
+		int m=sc.nextInt();
+		System.out.println("Enter number of columns in matrix");
+		int n=sc.nextInt();
 		System.out.println("Row=" + m + ", Col=" + n + ", number of paths = " + numberOfPaths(m,n));
 
 	}
@@ -75,6 +74,8 @@ public class number_of_paths
 	/***
 
 	Code complexity: O(MXN)
+	M = Number of rows in matrix
+	N = Number of columns in matrix
 
 	Input: 
 		T1: rows=3, columns=7
